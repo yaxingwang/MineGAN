4ee122b65d5932fe4e1df57c33fd2fe548cf1074
yaxingwang <yaxing@cvc.uab.es>
2020-07-01 16:36:11 +0200

    Initial commit

---
+++ styleGAN/data_prepare.sh
@@ -0,0 +1,29 @@
+python precompute_acts.py --dataset DogHead
+python prepare_data.py --dataset DuckHead  --n_worker 8 
+python precompute_acts.py --dataset DuckHead
+python prepare_data.py --dataset EagleHead  --n_worker 8 
+python precompute_acts.py --dataset EagleHead
+python prepare_data.py --dataset ElephantHead  --n_worker 8 
+python precompute_acts.py --dataset ElephantHead
+python prepare_data.py --dataset HumanHead  --n_worker 8 
+python precompute_acts.py --dataset HumanHead
+python prepare_data.py --dataset LionHead  --n_worker 8 
+python precompute_acts.py --dataset LionHead
+python prepare_data.py --dataset MonkeyHead  --n_worker 8 
+python precompute_acts.py --dataset MonkeyHead
+python prepare_data.py --dataset MouseHead  --n_worker 8 
+python precompute_acts.py --dataset MouseHead
+python prepare_data.py --dataset PandaHead  --n_worker 8 
+python precompute_acts.py --dataset PandaHead
+python prepare_data.py --dataset PigeonHead  --n_worker 8 
+python precompute_acts.py --dataset PigeonHead
+python prepare_data.py --dataset PigHead  --n_worker 8 
+python precompute_acts.py --dataset PigHead 
+python prepare_data.py --dataset RabbitHead  --n_worker 8 
+python precompute_acts.py --dataset RabbitHead
+python prepare_data.py --dataset SheepHead  --n_worker 8 
+python precompute_acts.py --dataset SheepHead
+python prepare_data.py --dataset TigerHead  --n_worker 8 
+python precompute_acts.py --dataset TigerHead
+python prepare_data.py --dataset WolfHead  --n_worker 8 
+python precompute_acts.py --dataset WolfHead
